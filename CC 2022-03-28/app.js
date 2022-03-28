d21033fdf5a0cc787974465bd958b26756705208
Larry Nguyen <larrynguyen771@gmail.com>
2022-03-28 11:32:54 -0500

    Coding challenge and solution for 3-28-2022 coding challenge

---
+++ CC 2022-03-28/app.js
@@ -0,0 +1,16 @@
+// Very simple, given an integer or a floating-point number, find its opposite.
+
+// Examples:
+
+// 1: -1
+// 14: -14
+// -34: 34
+
+function opposite(number) {
+    //using math.sign() to determine if number is negative 
+    if (Math.sign(number) === -1){
+      return number * -1
+    }else{
+      return number * -1
+    }
+  }
