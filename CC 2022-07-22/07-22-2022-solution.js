d3563fe1c8284e8e5c6adf79ec2f58130558ba33
Larry Nguyen <larrynguyen771@gmail.com>
2022-07-22 20:37:35 -0500

    Coding challenge and solution for 07-22-2022

---
+++ CC 2022-07-22/07-22-2022-solution.js
@@ -0,0 +1,16 @@
+// No oddities here
+// DESCRIPTION:
+// Write a small function that returns the values of an array that are not odd.
+
+// All values in the array will be integers. Return the good values in the order they are given.
+
+function noOdds( values ){
+    // Return all non-odd values
+    let even = []
+    values.forEach((e) =>{
+      if(e % 2 === 0){
+        even.push(e)
+      }
+    })
+    return even
+  }
