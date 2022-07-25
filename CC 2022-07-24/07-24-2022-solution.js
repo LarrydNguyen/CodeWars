9eb60fe1bb776c4723dc4ec216648d6621311239
Larry Nguyen <larrynguyen771@gmail.com>
2022-07-24 20:42:55 -0500

    Coding challenge and solution for 07-24-2022

---
+++ CC 2022-07-24/07-24-2022-solution.js
@@ -0,0 +1,9 @@
+// Grasshopper - Variable Assignment Debug
+// DESCRIPTION:
+// Variable assignment
+// Fix the variables assigments so that this code stores the string 'devLab' in the variable name.
+
+var a = "dev"
+var b = "Lab"
+
+var name = `${a}${b}`
