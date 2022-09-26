e3775a8683be613c8ad48ad3dc5f03e1e1dc3a29
Larry Nguyen <larrynguyen771@gmail.com>
2022-09-25 20:56:34 -0500

    Adding coding challenge

--- CC 2022-09-06/09-06-2022-solution.js
+++ CC 2022-09-06/09-06-2022-solution.js
@@ -1,4 +1,5 @@
 // Sort arrays - 1
+//description
 // Just a simple sorting usage. Create a function that returns the elements of the input-array / list sorted in lexicographical order.
 
 // input: names - unsorted array
