4a00e6d25f2b3c44330f398bcf70e02f55b7f9f0
Larry Nguyen <95257196+LarrydNguyen@users.noreply.github.com>
2022-10-25 10:05:36 -0500

    Coding challenge and solution for Huntober-day-7

---
+++ CC 2022-10-25/Huntober-day-7.js
@@ -0,0 +1,24 @@
+// Return the number of unique arrays that can be formed by picking exactly one element from each subarray.
+
+// For example: solve([[1,2],[4],[5,6]]) = 4, because it results in only 4 possibilites. They are [1,4,5],[1,4,6],[2,4,5],[2,4,6].
+
+// Make sure that you don't count duplicates; for example solve([[1,2],[4,4],[5,6,6]]) = 4, since the extra outcomes are just duplicates.
+
+// multiple arrays, no negatives, no floats
+// number of arrays that can be formed 
+
+function solve(arr){
+    console.log(arr.map(v => [...new Set(v)].length))
+    // for each val, make a new Set(for no duplicates) of array with only size
+    // reduce() to multiply each array to determine how many arrays can be formed. 
+  return arr.map(v => [...new Set(v)].length).reduce((a,c)=>a*c,1)
+  }
+  
+  // function solve(arr){
+  // return arr.map(v => new Set(v).size).reduce((a,c)=>a*c,1)
+  // }
+  
+  console.log(solve([[1,2],[4],[5,6]]),4)
+  console.log(solve([[1,2],[4,4],[5,6,6]]),4)
+  console.log(solve([[1,2],[3,4],[5,6]]),8)
+  console.log(solve([[1,2,3],[3,4,6,6,7],[8,9,10,12,5,6]]),72)
